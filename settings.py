e22981900fae7c864ce09d2848f776f2e2c0f597
Michael Ziegler <diese-addy@funzt-halt.net>
2013-03-31 14:12:30 +0200

    adapt static URL stuff for django 1.4

--- settings.py
+++ settings.py
@@ -50,12 +50,13 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/static/'
+STATIC_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'jw5t##@@e7-^hmq-d7tn(@t0&cl=+enpk5#+5&4f+kj6d0c(6r'
